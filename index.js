31686a4207e30d24272ee47d1712b359e7b51ef5
Qaisar_Azeemi <qaisarazeemi@uop.edu.pk>
2021-11-09 16:22:27 +0500

    deployment app  updated

--- index.js
+++ index.js
@@ -3,6 +3,60 @@ var app = express();
 
 var port = process.env.PORT || 8088
 app.use(express.json());
+var appliances = [
+    { id: 1, name: "Juicer", quantity: 3 },
+    { id: 2, name: "Washig machine", quantity: 2 },
+    { id: 3, name: "owen", quantity: 1 }
+];
+
+app.use(express.json()) // middlewear called to interect with outer world via post. i will connect your machine 
+    // with the server
+
+app.get("/appliances", (request, response) => {
+    //response.send("This is the list of students " + JSON.stringify(students));
+    var result = `<table border=2 >
+                    <tr>
+                    <th>SN</th>
+                    <th>NAME</th>
+                    <th>Quantity</th>
+                    </tr>`
+    appliances.map((value) => {
+        result += `<tr>
+                    <td>${value.id}</td>
+                    <td>${value.name}</td>
+                    <td>${value.quantity}</td>
+                    </tr>`
+
+    })
+    response.send(result)
+})
+app.post("/appliances", (request, responses) => {
+    var appliance = {
+        id: appliances.length + 1,
+        name: request.body.name,
+        quantity: request.body.quantity
+    }
+    appliances.push(appliance)
+    response.send("Data is added to the main streem")
+})
+app.put("/appliance/:id", (request, response) => {
+
+    var appliance = appliances.find(i => i.id === parseInt(request.params.id))
+        // var index = students.indexOf(student)
+        // students.splice(index, 1)
+    response.send("The id is this : " + appliance)
+    appliance.name = request.body.name
+    response.send("Index is Updated")
+})
+
+app.delete("/appliance/:id", (request, response) => {
+
+    var appliance = appliancs.find(i => i.id === parseInt(request.params.id))
+    var index = appliancs.indexOf(student)
+    appliancs.splice(index, 1)
+    response.send("Record is Deleted")
+})
+
 app.get("/", (request, response) => {
     response.send("This is my first Heroku deployment app running on port " + port);
 })
